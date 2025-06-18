ab48e3b23abc5b5a6cb90612c9550ab695d18614
restuak <restuaka.works@gmail.com>
2025-06-19 02:56:06 +0700

    update

--- module-2/4-cssframework/src/app/layout.tsx
+++ module-2/4-cssframework/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
 import Bg from "@/components/bg/bg";
 
 const geistSans = Geist({
@@ -30,8 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children }
-
+        {children}
       </body>
     </html>
   );
--- module-2/4-cssframework/src/app/page.tsx
+++ module-2/4-cssframework/src/app/page.tsx
@@ -7,16 +7,16 @@ export default function Home() {
         @import
         url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap');
       </style>
-      <div class="bg-white">
-        <img src="./img/bg.png" alt="Background" class="image grad-bg" />
+      <div className="bg-white">
+        <img src="./img/bg.png" alt="Background" className="image grad-bg" />
 
         {/* Content */}
-        <div class="relative flex flex-col items-center pt-4 px-4 ">
+        <div className="relative flex flex-col items-center pt-4 px-4 ">
           {/* TITLE */}
-          <div class="flex justify-between items-center w-[325px] max-w-xl text-white mb-8 md:w-[540px]">
-            <p class="josefin-tit">TODO</p>
+          <div className="flex justify-between items-center w-[325px] max-w-xl text-white mb-8 md:w-[540px]">
+            <p className="josefin-tit">TODO</p>
             <button>
-              <img src="./img/light.svg"></img>
+              <img src="./img/light.svg" className="hover:cursor-pointer"></img>
             </button>
           </div>
 
@@ -24,93 +24,120 @@ export default function Home() {
           <input
             type="checkbox text"
             placeholder="Create a new todo..."
-            class="josefin-create focus:text-[#000000] w-[327px] md:w-[540px] max-w-xl p-4 rounded-md bg-white shadow-lg mb-4 focus:outline-none"
+            className="josefin-create focus:text-[#000000] w-[327px] md:w-[540px] max-w-xl p-4 rounded-md bg-white shadow-lg mb-4 focus:outline-none"
           />
           {/* Todo Box */}
-          <div class="w-[327px] md:w-[540px] max-w-xl bg-white shadow-lg rounded-md overflow-hidden">
-            <ul class="divide-y divide-gray-200">
-              <li class="flex items-center justify-between p-4">
-                <div class="flex items-center gap-3">
+          <div className="w-[327px] md:w-[540px] max-w-xl bg-white shadow-lg rounded-md overflow-hidden">
+            <ul className="divide-y divide-gray-200">
+              <li className="flex items-center justify-between p-4">
+                <div className="flex items-center gap-3">
                   <input
                     type="checkbox"
                     checked
-                    class="accent-violet-600 w-5 h-5"
+                    className="accent-violet-600 w-5 h-5"
                   />
-                  <span class="josefin-list2">
+                  <span className="josefin-list2 hover:cursor-pointer">
                     Complete online JavaScript course
                   </span>
                 </div>
-                <button class="text-[#D1D2DA]">✕</button>
+                <button className="text-[#D1D2DA] hover:text-[#000000] hover:cursor-pointer">
+                  ✕
+                </button>
               </li>
-              <li class="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
-                <div class="flex items-center gap-3">
-                  <input type="checkbox" class="g w-5 h-5" />
-                  <span class=" josefin-list">Jog around the park 3x</span>
+              <li className="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
+                <div className="flex items-center gap-3">
+                  <input
+                    type="checkbox"
+                    className="accent-violet-600 w-5 h-5"
+                  />
+                  <span className=" josefin-list">Jog around the park 3x</span>
                 </div>
-                <button class="text-[#D1D2DA]">✕</button>
+                <button className="text-[#D1D2DA] hover:text-[#000000] min-sm:hidden hover:cursor-pointer">
+                  ✕
+                </button>
               </li>
-              <li class="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
-                <div class="flex items-center gap-3 ">
-                  <input type="checkbox" class="accent-violet-600 w-5 h-5" />
-                  <span class="josefin-list ">10 minutes meditation</span>
+              <li className="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
+                <div className="flex items-center gap-3 ">
+                  <input
+                    type="checkbox"
+                    className="accent-violet-600 w-5 h-5"
+                  />
+                  <span className="josefin-list ">10 minutes meditation</span>
                 </div>
-                <button class="text-[#D1D2DA]">✕</button>
+                <button className="text-[#D1D2DA] hover:text-[#000000] min-sm:hidden hover:cursor-pointer">
+                  ✕
+                </button>
               </li>
-              <li class="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
-                <div class="flex items-center gap-3">
-                  <input type="checkbox" class="accent-violet-600 w-5 h-5" />
-                  <span class=" josefin-list">Read for 1 hour</span>
+              <li className="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
+                <div className="flex items-center gap-3">
+                  <input
+                    type="checkbox"
+                    className="accent-violet-600 w-5 h-5"
+                  />
+                  <span className=" josefin-list">Read for 1 hour</span>
                 </div>
-                <button class="text-[#D1D2DA]">✕</button>
+                <button className="text-[#D1D2DA] hover:text-[#000000] min-sm:hidden hover:cursor-pointer">
+                  ✕
+                </button>
               </li>
-              <li class="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
-                <div class="flex items-center gap-3">
-                  <input type="checkbox" class="accent-violet-600 w-5 h-5" />
-                  <span class=" josefin-list">Pick up groceries</span>
+              <li className="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
+                <div className="flex items-center gap-3">
+                  <input
+                    type="checkbox"
+                    className="accent-violet-600 w-5 h-5"
+                  />
+                  <span className=" josefin-list">Pick up groceries</span>
                 </div>
-                <button class="text-[#D1D2DA]">✕</button>
+                <button className="text-[#D1D2DA] hover:text-[#000000] min-sm:hidden hover:cursor-pointer">
+                  ✕
+                </button>
               </li>
-              <li class="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
-                <div class="flex items-center gap-3">
-                  <input type="checkbox" class="accent-violet-600 w-5 h-5" />
-                  <span class=" josefin-list">
+              <li className="flex items-center justify-between p-4 gap-3 hover:bg-gray-50 hover:cursor-pointer">
+                <div className="flex items-center gap-3">
+                  <input
+                    type="checkbox"
+                    className="accent-violet-600 w-5 h-5"
+                  />
+                  <span className=" josefin-list">
                     Complete Todo App on Frontend Mentor
                   </span>
                 </div>
-                <button class="text-[#D1D2DA]">✕</button>
+                <button className="text-[#D1D2DA] hover:text-[#000000] min-sm:hidden hover:cursor-pointer">
+                  ✕
+                </button>
               </li>
               <li></li>
             </ul>
 
             {/* Filter/Footer */}
-            <div class="josefin-create flex justify-between items-center p-4 gap-4">
+            <div className="josefin-create flex justify-between items-center p-4 gap-4">
               <span>5 items left</span>
-              <div class="max-sm:hidden md:flex justify-center gap-4">
-                <button class="text-[#3A7CFD] font-semibold">All</button>
-                <button class="hover:text-[#000000] font-semibold">
+              <div className="max-sm:hidden md:flex justify-center gap-4">
+                <button className="text-[#3A7CFD] font-semibold">All</button>
+                <button className="hover:text-[#000000] font-semibold">
                   Active
                 </button>
-                <button class="hover:text-[#000000] cursor-pointer font-semibold">
+                <button className="hover:text-[#000000] cursor-pointer font-semibold">
                   Completed
                 </button>
               </div>
-              <button class="active:text-black cursor-pointer">
+              <button className="active:text-black cursor-pointer">
                 Clear Completed
               </button>
             </div>
           </div>
           <br />
-          <div class="min-sm:hidden josefin-bot w-[327px] md:w-[540px] max-w-xl bg-white shadow-lg rounded-md space-x-4 flex items-center justify-center p-4 gap-4">
-            <button class="text-[#3A7CFD] font-semibold">All</button>
-            <button class="hover:text-[#000000] cursor-pointer font-semibold">
+          <div className="min-sm:hidden josefin-bot w-[327px] md:w-[540px] max-w-xl bg-white shadow-lg rounded-md space-x-4 flex items-center justify-center p-4 gap-4">
+            <button className="text-[#3A7CFD] font-semibold">All</button>
+            <button className="hover:text-[#000000] cursor-pointer font-semibold">
               Active
             </button>
-            <button class="hover:text-[#000000] cursor-pointer font-semibold">
+            <button className="hover:text-[#000000] cursor-pointer font-semibold">
               Completed
             </button>
           </div>
 
-          <p class="text-sm josefin3 mt-6">Drag and drop to reorder list</p>
+          <p className="text-sm josefin3 mt-6">Drag and drop to reorder list</p>
         </div>
       </div>
     </main>
