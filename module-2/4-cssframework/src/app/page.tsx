e5dd76389f9ba765a3376eb3204e670a053b9c94
restuak <restuaka.works@gmail.com>
2025-06-18 13:47:13 +0700

    update

---
+++ module-2/4-cssframework/src/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div className="flex flex-col justify-center">
+        <img src="/img/tdl-bg1.jpg"></img>
+        <main className=""></main>
+      </div>
+      <div class="border-2 border-solid border-\[327px\] rounded-[5px] justify-center">
+        Create a new todo
+      </div>
+      width: 327; height: 48; top: 108px; left: 24px;
+    </main>
+  );
+}
---
+++ module-2/4-cssframework/src/components/todolist/todolist.tsx
@@ -0,0 +1,12 @@
+import "./todolist.css";
+export default function Todolist() {
+  return (
+    <div className="h12 p-2.5 bg-black text-white flex justify-between p-2.5">
+      <div className="text-3x1"> LOGO </div>
+      <div className="flex gap-5 mr-5">
+        <button className="button figtree-normal">Login</button>
+        <button className="button figtree-normal">Register</button>
+      </div>
+    </div>
+  );
+}
